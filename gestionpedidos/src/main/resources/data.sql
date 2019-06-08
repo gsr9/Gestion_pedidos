3766d601b517da113ab1cb1ee2557559e7091ce3
gsr9 <gsr9@alu.ua.es>
2019-06-08 20:15:48 +0200

    Prepare data, spring boot jar and npm commands

--- gestionpedidos/src/main/resources/data.sql
+++ gestionpedidos/src/main/resources/data.sql
@@ -1,13 +1,12 @@
 /* Users */
 insert into user_table (id, name, email, pass) values
-(1, 'Bilbo Baggins', 'bilbo@lotr.com', '1234'),
-(2, 'Frodo Baggins', 'frodo@lotr.com', '1234');
+(1, 'Administrador', 'admin@admin', 'admin');
 
 
 /* Orders */
 insert into order_table (id, first_plate, second_plate, dessert, date, user_id, state) values
 (1, 'Ensalada murciana', 'Fideuá', 'Fruta de temporada', '2019-06-05', 1, 0),
-(2, 'Ensalada alicantina', 'Paella de pollo', 'Brownie', '2019-06-05', 2, 1);
+(2, 'Ensalada alicantina', 'Paella de pollo', 'Brownie', '2019-06-05', 1, 1);
 
 
 /* Plates */
